7f186dd50648c85fcc4b1f59482738aa7d9edc53
Dante <130088266+Dantrux22@users.noreply.github.com>
2025-05-19 19:37:51 -0300

    Add files via upload

---
+++ scrips/load-header.js
@@ -0,0 +1,13 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+      fetch("/pages/header.html")
+          .then(response => response.text())
+          .then(data => {
+              document.getElementById("header-placeholder").innerHTML = data;
+          })
+          .catch(error => console.error("Error al cargar el header:", error));
+  });
+
+  function toggleMenu() {
+    document.getElementById("mobileMenu").classList.toggle("show");
+  }
