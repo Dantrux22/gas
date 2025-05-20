c4cb08c2cfccb4850947cbc237f86dcd69c14006
Dantrux22 <130088266+Dantrux22@users.noreply.github.com>
2025-05-19 21:27:03 -0300

    .

--- scrips/load-header.js
+++ scrips/load-header.js
@@ -1,12 +1,13 @@
 
-  document.addEventListener("DOMContentLoaded", function () {
-      fetch('pages/header.html')
-          .then(response => response.text())
-          .then(data => {
-              document.getElementById("header-placeholder").innerHTML = data;
-          })
-          .catch(error => console.error("Error al cargar el header:", error));
-  });
+// load-header.js
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('./components/header.html')  // O '../components/header.html' si estás en una subcarpeta
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        })
+        .catch(error => console.error("Error al cargar el header:", error));
+});
 
   function toggleMenu() {
     document.getElementById("mobileMenu").classList.toggle("show");
